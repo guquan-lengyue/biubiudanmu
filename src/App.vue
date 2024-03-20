77033d389114f9c24543b973653fe48a081443e7
孤泉冷月 <guquanlengyue@gmail.com>
2024-03-20 08:39:42 +0000

    test page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { DanmuClient } from '@/lib/contract/client/danmu'
-import type { Danmu } from '@/lib/contract/types/danmu'
+import type { DanmuContent, Danmu } from '@/lib/contract/types/danmu'
 
-let danmu = ref<Danmu>({
+let danmu = ref<DanmuContent>({
   txt: '',
   t: '',
   fs: '',
@@ -16,34 +16,30 @@ function sendDanmu() {
   DanmuClient.send(danmu.value, { targetCharId: 66442, noteId: 1 })
 }
 
-
 let danmuList = ref<Danmu[]>()
 
 async function findVideo() {
   danmuList.value = await DanmuClient.findDanmu({ targetCharId: 66442, noteId: 1 })
 }
-
 </script>
 
 <template>
   <div>
-    <div>txt: <input type="text" v-model="danmu.txt"></div>
-    <div>t: <input type="text" v-model="danmu.t"></div>
-    <div>fs: <input type="text" v-model="danmu.fs"></div>
-    <div>fc: <input type="text" v-model="danmu.fc"></div>
-    <div>pt: <input type="text" v-model="danmu.pt"></div>
-    <div>lang?: <input type="text" v-model="danmu.lang"></div>
+    <div>txt: <input type="text" v-model="danmu.txt" /></div>
+    <div>t: <input type="text" v-model="danmu.t" /></div>
+    <div>fs: <input type="text" v-model="danmu.fs" /></div>
+    <div>fc: <input type="text" v-model="danmu.fc" /></div>
+    <div>pt: <input type="text" v-model="danmu.pt" /></div>
+    <div>lang?: <input type="text" v-model="danmu.lang" /></div>
     <div>{{ danmu }}</div>
     <button @click="sendDanmu">发送</button>
   </div>
   <div>
     <button @click="findVideo">查找弹幕</button>
-    <div v-for="(a,index) in danmuList" :key="index">
-      {{ index }} ---{{ a }}
+    <div v-for="(a, index) in danmuList" :key="a.characterId + a.noteId">
+      {{ index }} ---{{ a }} <button @click="DanmuClient.delDanmu(a.noteId)">删除</button>
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
