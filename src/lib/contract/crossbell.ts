8483ef8f1f870301ca1d5d9f5c49d580544eede9
孤泉冷月 <56825011+guquan-lengyue@users.noreply.github.com>
2024-03-20 07:34:40 +0000

    添加crossbell note 的增删改查方法 add crossbel note crud function

--- src/lib/contract/crossbell.ts
+++ src/lib/contract/crossbell.ts
@@ -1,7 +1,6 @@
 import { Contract, createContract, createIndexer, Indexer } from 'crossbell'
 import { type Address } from 'abitype'
 
-
 export class Client {
   protected address?: Address
   protected chartId?: number
@@ -21,7 +20,6 @@ export class Client {
    * @private
    */
   private async initChar() {
-
     const addresses = await this.contract.walletClient.requestAddresses()
     this.address = addresses[0]
     if (!this.address) {
@@ -43,4 +41,67 @@ export class Client {
     console.info(log)
   }
 
+  /**
+   * post a note and link it to another note
+   * @param targetCharacterId another note author characterId
+   * @param targetNoteId another note id
+   * @param sources source type
+   * @param content note content
+   */
+  public async postNoteForNote(
+    targetCharacterId: number,
+    targetNoteId: number,
+    sources: string[],
+    content: string,
+    title?: string
+  ) {
+    const { data } = await this.contract.note.postForNote({
+      characterId: this.chartId!,
+      metadataOrUri: { content: JSON.stringify(content), sources, title },
+      targetCharacterId,
+      targetNoteId
+    })
+    return data
+  }
+
+  /**
+   * post a note
+   * @param sources source type
+   * @param content note connect
+   * @returns noteId
+   */
+  public async postNote(sources: string[], content: string, title?: string) {
+    const { data } = await this.contract.note.post({
+      characterId: this.chartId!,
+      metadataOrUri: { sources, content, title }
+    })
+    return data
+  }
+
+  /**
+   * edit note metadata
+   * @param nodeId  node id
+   * @param content the modify content 
+   * @param title the modify title
+   */
+  public async editNoteMetaData(
+    nodeId: number,
+    content: string,
+    title?: string
+  ) {
+    this.contract.note.changeMetadata({
+      characterId: this.chartId!,
+      noteId: nodeId,
+      modifier: (s) => ({ sources: s?.sources, content, title })
+    })
+  }
+
+  /**
+   * change the note's property deleted to true
+   * @param nodeId the noteId
+   * @returns
+   */
+  public async deleteNote(nodeId: number) {
+    return await this.contract.note.delete({ characterId: this.chartId!, noteId: nodeId })
+  }
 }
